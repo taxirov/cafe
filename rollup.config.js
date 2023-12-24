9814e28fe40546f0d6d9568e124403a5471ebde7
Saad Takhir <saadtakhir1@gmail.com>
2023-12-24 12:05:10 +0500

    sad

--- rollup.config.js
+++ rollup.config.js
@@ -35,6 +35,7 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
+		sourceMap: false,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js'
--- src/components/CategoryComponent.svelte
+++ src/components/CategoryComponent.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type { Category } from "./src/store";
+    import type { Category } from "../store";
 
     export let category: Category
 
