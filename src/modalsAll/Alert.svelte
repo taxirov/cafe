7133d4d7b00b8e7f9a23f2be79c4a1c9274a5004
Saad Takhir <saadtakhir1@gmail.com>
2023-12-25 21:17:50 +0500

    alert width update

--- src/modalsAll/Alert.svelte
+++ src/modalsAll/Alert.svelte
@@ -7,8 +7,8 @@
     export let close: () => {}
 </script>
 
-<div class={"w-screen h-screen bg-black/20 fixed justify-center items-center " + (show ? "flex" : "hidden")}>
-    <div class="flex flex-col items-center gap-3 bg-white rounded-2xl p-3 w-4/5 md:w-fit">
+<div class={"w-screen h-screen bg-black fixed justify-center items-center " + (show ? "flex" : "hidden")}>
+    <div class="flex flex-col items-center gap-3 bg-white rounded-2xl p-5 w-4/5 md:w-1/5">
         <span class={"w-[64px] h-[64px] flex items-center justify-center rounded-[100px] bg-" + color}><i class="bi bi-{icon} text-[40px] text-gray-100"></i></span>
         <p class="text-3xl font-semibold">{title}</p>
         <p class="text-sm font-medium text-center">{text}</p>
